130de05d300993505816666b16b3c63220029a23
Terbuh <hubertt.popowicz@gmail.com>
2023-08-08 19:57:23 +0200

    modified

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,40 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { RobText } from "../styledComponents";
-import { LinearGradient } from "expo-linear-gradient";
 
 const HistoryScreen = ({ route }) => {
     const { historyData } = route.params || {};
     const [updatedHistoryData, setUpdatedHistoryData] = useState(historyData);
-
-    const handleDeleteItem = (index) => {
-        const newHistoryData = [...updatedHistoryData];
-        newHistoryData.splice(index, 1);
-        setUpdatedHistoryData(newHistoryData);
-    };
+    const [monthlyIncomes, setMonthlyIncomes] = useState({});
 
     const formatDate = (date) => {
         const formattedDate = new Date(date);
         const day = formattedDate.getDate();
-        const month = formattedDate.getMonth() + 1; // Uwaga: Miesiące są indeksowane od 0
+        const month = formattedDate.toLocaleString("default", { month: "long" });
         const year = formattedDate.getFullYear();
         return `${day}-${month}-${year}`;
     };
 
-    return (
+    const calculateTotalIncome = () => {
+        return updatedHistoryData.reduce((total, item) => total + item.income, 0);
+    };
 
+    const updateMonthlyIncomes = () => {
+        const updatedMonthlyIncomes = {};
+
+        for (const item of updatedHistoryData) {
+            const date = new Date(item.date);
+            const monthYear = `${date.toLocaleString("default", { month: "long" })}-${date.getFullYear()}`;
+
+            if (!updatedMonthlyIncomes[monthYear]) {
+                updatedMonthlyIncomes[monthYear] = 0;
+            }
+
+            updatedMonthlyIncomes[monthYear] += item.income;
+        }
+
+        setMonthlyIncomes(updatedMonthlyIncomes);
+    };
+
+    useEffect(() => {
+        setUpdatedHistoryData(historyData);
+        updateMonthlyIncomes();
+    }, [historyData]);
+
+    return (
         <View style={styles.container}>
             {updatedHistoryData.map((item, index) => (
                 <View key={index} style={styles.historyItem}>
                     <RobText>Date: {formatDate(item.date)}</RobText>
                     <RobText>Income: {item.income}</RobText>
-                    <TouchableOpacity onPress={() => handleDeleteItem(index)}>
-                        <RobText style={styles.deleteButton}>Usuń</RobText>
-                    </TouchableOpacity>
                 </View>
             ))}
+            <View style={styles.totalIncomeContainer}>
+                <RobText style={styles.totalIncomeText}>
+                    Total Income: {calculateTotalIncome()}
+                </RobText>
+            </View>
+            <View style={styles.monthlyIncomesContainer}>
+                {Object.keys(monthlyIncomes).map((monthYear) => (
+                    <View key={monthYear} style={styles.monthlyIncomeItem}>
+                        <RobText>{monthYear}:</RobText>
+                        <RobText>{monthlyIncomes[monthYear]}</RobText>
+                    </View>
+                ))}
+            </View>
         </View>
-
     );
 };
 
@@ -42,7 +70,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
-        height: '100%'
     },
     historyItem: {
         backgroundColor: "#ecf0f1",
@@ -52,9 +79,23 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
     },
-    deleteButton: {
-        color: "red",
+    totalIncomeContainer: {
+        marginTop: 20,
+        alignItems: "center",
+    },
+    totalIncomeText: {
+        fontSize: 18,
         fontWeight: "bold",
+    },
+    monthlyIncomesContainer: {
+        marginTop: 20,
+        alignItems: "center",
+    },
+    monthlyIncomeItem: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: 200,
+        marginBottom: 10,
     },
 });
 
